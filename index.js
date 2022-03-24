59ce6c944af285212c460b2b89dcd7100f3ac796
karta2341940 <a08487a@gmail.com>
2022-03-24 17:09:33 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let form = document.getElementsByTagName('form')[0];
-
+let input =document.getElementById('inputText');
 
 function showAlert(){
     let alert = document.querySelector('.alert');
@@ -11,14 +11,22 @@ function showAlert(){
 
 function synthesize(sentense){
     //To do : Connect to Server and parse request
+    let URL ='';
+    fetch(URL,{
+        method: 'GET'
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     //register add submit event
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+        if( input.value == "")
         showAlert();
+        else 
         console.log('hi');
+        input.value="";
     });
 
 });
