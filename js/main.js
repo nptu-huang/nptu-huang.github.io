42680308936e4125926a2c2f2c0f1b3621bdffb6
karta2341940 <a08487a@gmail.com>
2022-04-18 16:48:09 +0800

    base64 OK

--- js/main.js
+++ js/main.js
@@ -25,15 +25,25 @@ async function delay(t)
 }
 
 async function testFetch(){
+    console.clear()
     let request = {
-        message: "jin1 tian1 ou1 yin4"
-    };
-
-    let result =await fetch(server+"download", {
-        method: "GET",
+        message: "jin1 tian1 ou1 yin4 ming2 tian1 fa1 cai2 hou4 tian1 jiu4 tui4 xiu1"
+    }
+    result = await fetch(server+"test", {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
         redirect: 'follow',
-        
+        body: JSON.stringify(request)
     });
     
+    let data = await result.json();
+    let len = String(data['voice']).length
+    png = "data:audio/wav;base64,"+String(data['voice']).substring(2,len-1)
+    console.log(png);
+
+
+    document.querySelector('audio').src=png;
     
 }
