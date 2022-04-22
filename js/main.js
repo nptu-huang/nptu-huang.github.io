6b082ce745d0e2bf31de98dbb75806e88d226805
karta2341940 <a08487a@gmail.com>
2022-04-22 10:16:06 +0800

    update token

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ async function test(){
             'Content-Type': 'application/json',
             "Access-Control-Request-Private-Network": true,
             "Authentication": 'secret',
-            "Oringin-Trial":"AstmEHS3busYlixToMZbeRahTB50yq9A+raC40ZGJMK2Aimi+oHyR4fT5wuKHBsGywLlTh3GPCeK2KHOJjnSIAMAAAB3eyJvcmlnaW4iOiJodHRwOi8vZ2l0LnNoYXJrLXNtYWxsLmNvbTo4MCIsImZlYXR1cmUiOiJQcml2YXRlTmV0d29ya0FjY2Vzc05vblNlY3VyZUNvbnRleHRzQWxsb3dlZCIsImV4cGlyeSI6MTY2NjEzNzU5OX0=",
+            "Oringin-Trial":"Avrro/wjXfLjIr0odQk+sfyVUOhG7qOa3XKtJvgRRqKa23OT60Nb2L50BG0MgZxEajQCjWXrP6H2G9T+AWsE6Q8AAAByeyJvcmlnaW4iOiJodHRwOi8vMTcyLjI5LjcuMTE1OjkwMDAiLCJmZWF0dXJlIjoiUHJpdmF0ZU5ldHdvcmtBY2Nlc3NOb25TZWN1cmVDb250ZXh0c0FsbG93ZWQiLCJleHBpcnkiOjE2NjYxMzc1OTl9",
 
         },
         mode:"cors",
