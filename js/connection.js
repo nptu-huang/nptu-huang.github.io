ed0ca714a0a88c4ba3920bd56e1e8f0a86d42b2a
karta2341940 <a08487a@gmail.com>
2022-04-23 22:40:00 +0800

    update

--- js/connection.js
+++ js/connection.js
@@ -24,10 +24,7 @@ async function runMode() {
                 method: 'GET',
                 signal: signal,
                 headers: {
-                    'Content-Type': 'application/json',
-                    "Oringin-Trial":"AstmEHS3busYlixToMZbeRahTB50yq9A+raC40ZGJMK2Aimi+oHyR4fT5wuKHBsGywLlTh3GPCeK2KHOJjnSIAMAAAB3eyJvcmlnaW4iOiJodHRwOi8vZ2l0LnNoYXJrLXNtYWxsLmNvbTo4MCIsImZlYXR1cmUiOiJQcml2YXRlTmV0d29ya0FjY2Vzc05vblNlY3VyZUNvbnRleHRzQWxsb3dlZCIsImV4cGlyeSI6MTY2NjEzNzU5OX0=",
-                    "Access-Control-Request-Private-Network": "true",
-                    
+                    'Content-Type': 'application/json',  
                 },
                 redirect: 'follow',
             });
