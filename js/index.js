7f432da3a88cf1d7ecd8836b74f80ae148102ef6
karta2341940 <a08487a@gmail.com>
2022-05-03 22:07:08 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -39,45 +39,49 @@ document.addEventListener('DOMContentLoaded', () => {
         
 
     });
-    document.querySelector('form:nth-child(2)').addEventListener('submit',async(e)=>{
+    document.querySelectorAll('form')[1].addEventListener('submit',async(e)=>{
         e.preventDefault();
         if (input2.value == "") {
             showAlert();
             return
         }
-        let data = await playSend2(input.value)
-        setItem(data);
+        let data = await playSend(input2.value)
+        setItem2(data);
 
     });
 
 });
 
 async function playSend(Msg) {
-    loadText.innerHTML = "Loading"
+    loadText.innerHTML = "Loading";
     loading.forEach(i => { i.classList.remove('hide') });
     let request = {
-        message: Msg
+        message: `${Msg}`
     }
-    let result = fetch(server, {
-        method: 'POST',
+    console.log(request.message)
+    let result = await fetch(server+"text", {
+        method: 'post',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin":true
         },
         redirect: 'follow',
         body: JSON.stringify(request)
-    })
-    result.then(res=>{
-        console.log("res",res);
-        res.headers.forEach(i=>{
-            console.log("i",i)
-        })
-    })
-    result = await (await result).blob();
-    let bUrl = URL.createObjectURL(result);
-    buffer[hash(Msg)] = bUrl;
+    });
+    
+    let data = await result.json();
+    
+    let len = String(data['voice']).length;
+    let wav = "data:audio/wav;base64,"+String(data['voice']).substring(2,len-1);
+    document.querySelector('audio').src=wav;
+    console.log(data["hakkaTone"])
     loading.forEach(i => { i.classList.add('hide') });
-    document.querySelector('audio').src = bUrl;
-    return bUrl;
+    return {
+        input:Msg,
+        hakkaStr:data["hakkaStr"]??"",
+        hakkaTone:data["hakkaTone"]
+
+    }
 
 }
 
@@ -132,6 +136,24 @@ function setItem(item={input:"",hakkaStr:"",hakkaTone:""}) {
         <!--<span class="task">客文字：${item.hakkaStr}</span><br>-->
         <span class="task">客文字：${item.input}</span><br>
         <span class="task">客語音標：${item.hakkaTone}</span>
+        </div>
+        <span class="task-item">
+            <span><i class="bi bi-volume-up-fill" onclick=playRecord("${hash(item.input)}") ></i></span>
+            <span><i class="bi bi-arrow-down" onclick=download("${hash(item.input)}") ></i></span>
+        </span>
+    </li>
+    `
+    listitem.insertAdjacentHTML("beforeend", str);
+
+}
+function setItem2(item={input:"",hakkaStr:"",hakkaTone:""}) {
+    tatal = [];
+    let listitem = document.querySelector('.list-group');
+    let str = listitem;
+    str = `
+    <li class="list-group-item d-flex justify-content-between align-items-center task-item-o">
+        <div>
+        <span class="task">客語音標：${item.input}</span>
         </div>
         <span class="task-item">
             <span><i class="bi bi-volume-up-fill" onclick=playRecord("${hash(item.input)}") ></i></span>
