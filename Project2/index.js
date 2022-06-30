33cff8ee8af4ddebbe9c86d83708bcadd40d05a2
karta2341940 <a08487a@gmail.com>
2022-06-30 17:24:46 +0800

    update(Vue)

---
+++ Project2/index.js
@@ -0,0 +1,36 @@
+const hakka = {
+    data() {
+        return {
+            items: [
+                {
+                    "chinese": "種田就跟讀書一樣",
+                    "hakka": "耕田同讀書",
+                    "pinyin": "gang24 tien11 tung11 tug5 su24",
+                    "voice": ""
+                },
+                {
+                    "chinese": "爸爸說我們客家人最重要的兩件事",
+                    "hakka": "阿爸講𠊎等客家人盡重要个兩件事",
+                    "pinyin": "a24 ba24 gong31 ngai11 den24 hag2 ga24 ngin11 qin55 cung55 ieu55 ge55 liong31 kian55 sii55",
+                    "voice": ""
+                }
+            ]
+        }
+
+    },
+    create() {
+
+    },
+    methods: {
+
+    },
+
+
+
+
+
+
+}
+
+
+Vue.createApp(hakka).mount('#app')
