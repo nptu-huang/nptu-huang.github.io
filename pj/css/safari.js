ab8ceb04d132a5099766a8f5dd7c08d23fbac7ad
karta2341940 <a08487a@gmail.com>
2022-07-12 15:42:30 +0800

    update test

--- pj/css/safari.js
+++ pj/css/safari.js
@@ -1,8 +1,10 @@
 function safariHacks() {
     let windowsVH = window.innerHeight / 100;
-    document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
-    window.addEventListener('resize', function() {
-        document.querySelector('.wrap').style.setProperty('--vh', windowsVH + 'px');
+    document.querySelector('.right').style.setProperty('--vh', windowsVH + 'px');
+    document.querySelector('.left').style.setProperty('--vh', windowsVH + 'px');
+    window.addEventListener('resize', function () {
+        document.querySelector('.right').style.setProperty('--vh', windowsVH + 'px');
+        document.querySelector('.left').style.setProperty('--vh', windowsVH + 'px');
     });
 }
 
