9f73b692b29f76c8cd4fef2512951dfac37810af
karta2341940 <a08487a@gmail.com>
2022-08-24 16:23:41 +0800

    update

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ const hakka = {
             play: "true",
             reportHide:true,
 
-            server: "http://server.nvda888.tk:9000",
+            server: "http:hcilab.nptu.edu.tw:9000",
 
         }
 
@@ -49,7 +49,7 @@ const hakka = {
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "http://server.nvda888.tk"
+                    "Access-Control-Allow-Origin": "http://server.nvda888.tk,http://hcilab.nptu.edu.tw"
                 },
                 redirect: 'follow',
                 body: JSON.stringify(request)
@@ -217,7 +217,7 @@ const hakka = {
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "http://server.nvda888.tk"
+                    "Access-Control-Allow-Origin": "http://server.nvda888.tk,http://hcilab.nptu.edu.tw"
                 },
                 redirect: 'follow',
                 body: JSON.stringify(request)
@@ -243,7 +243,7 @@ const hakka = {
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "http://server.nvda888.tk"
+                    "Access-Control-Allow-Origin": "http://server.nvda888.tk,http://hcilab.nptu.edu.tw"
                 },
                 redirect: 'follow',
                 body: JSON.stringify(request)
